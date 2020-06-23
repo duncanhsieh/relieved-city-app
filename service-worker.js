23f4ae0b18ba86696fbd4fa4109c354a82854e98
Duncan Hsieh <duncanhsieh@gmail.com>
2020-06-23 15:58:03 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/relieved-city-app/precache-manifest.9d7c68cae552153301dedc7010431344.js"
+  "/relieved-city-app/precache-manifest.541a4a78f43bd0c8a52d91583f279610.js"
 );
 
 self.addEventListener('message', (event) => {
