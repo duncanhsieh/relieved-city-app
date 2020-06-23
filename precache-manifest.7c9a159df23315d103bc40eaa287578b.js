a5f6c2f5efd5eece93033c361a295bedcb18fa34
Duncan Hsieh <duncanhsieh@gmail.com>
2020-06-23 14:23:34 +0800

    Updates

---
+++ precache-manifest.7c9a159df23315d103bc40eaa287578b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e7d1b721b0d5971c701bf9b352e897",
+    "url": "/relieved-city-app/index.html"
+  },
+  {
+    "revision": "821f7090cd89cd940c46",
+    "url": "/relieved-city-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/relieved-city-app/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/relieved-city-app/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821f7090cd89cd940c46",
+    "url": "/relieved-city-app/static/js/main.5eb3e78d.chunk.js"
+  },
+  {
+    "revision": "e20a5385f8203fdf387d",
+    "url": "/relieved-city-app/static/js/runtime-main.cfe07215.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/relieved-city-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
