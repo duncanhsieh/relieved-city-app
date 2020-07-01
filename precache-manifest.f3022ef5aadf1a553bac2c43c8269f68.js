61631b8fe1bec7175ccc05e63fb1639c16b2889c
Duncan Hsieh <duncanhsieh@gmail.com>
2020-07-01 14:13:42 +0800

    Updates

---
+++ precache-manifest.f3022ef5aadf1a553bac2c43c8269f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c059a1caea93124a8e70eaed0812a7ea",
+    "url": "/relieved-city-app/index.html"
+  },
+  {
+    "revision": "28a8bf8ff3ce82daec1b",
+    "url": "/relieved-city-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19640ebc39cde99af22b",
+    "url": "/relieved-city-app/static/js/2.df6e9efd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/relieved-city-app/static/js/2.df6e9efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a8bf8ff3ce82daec1b",
+    "url": "/relieved-city-app/static/js/main.6ade0f56.chunk.js"
+  },
+  {
+    "revision": "e20a5385f8203fdf387d",
+    "url": "/relieved-city-app/static/js/runtime-main.cfe07215.js"
+  }
+]);
